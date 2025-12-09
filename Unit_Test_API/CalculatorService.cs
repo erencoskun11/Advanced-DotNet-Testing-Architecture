3d1a5b5ac344283b3e5a55a123e31757455b6bb3
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-09 19:16:28 +0300

    feat(Service) : created for testing

---
+++ Unit_Test_API/CalculatorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Test_API
+{
+    public class CalculatorService: ICalculatorService
+    {
+        public int add2(int x, int y)
+        {
+            return x + y;
+        }
+    }
+  
+}
