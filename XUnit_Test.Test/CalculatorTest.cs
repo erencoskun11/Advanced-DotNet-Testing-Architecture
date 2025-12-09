063269c5bbfb33a0ea75d51909fb2e08fe8162da
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-09 16:20:25 +0300

    using Equal medhod

---
+++ XUnit_Test.Test/CalculatorTest.cs
@@ -0,0 +1,31 @@
+﻿using Unit_Test_API;
+
+namespace XUnit_Test.Test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            //Arrange
+
+            int x = 5;
+            int y = 20;
+
+            var calculator = new Calculator();
+
+
+
+            //Act
+            var total = calculator.add(x, y);
+
+
+            //Assert
+            
+            Assert.Equal<int>(25, total);
+
+
+
+        }
+    }
+}
