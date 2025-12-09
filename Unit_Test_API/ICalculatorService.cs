78ed4ead3fb630cb7261b31f1fcb5a9532e68ad0
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-09 19:17:35 +0300

    feat(IService) : created

---
+++ Unit_Test_API/ICalculatorService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Test_API
+{
+    public interface ICalculatorService
+    {
+        int add2(int x, int y);
+
+    }
+}
