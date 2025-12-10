82438933055044a6b112ded8d590b32b53cf2ae3
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-10 20:17:09 +0300

    feat(controller) : created

---
+++ Unit_Test_API/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Unit_Test_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // Bağımlılığı (Dependency) burada tanımlıyoruz
+        private readonly IRepository<Product> _repository;
+
+        // Constructor Injection (En önemli kısım burası!)
+        // Test yazarken buraya Mock nesnesi, gerçek çalışırken veritabanı nesnesi gelecek.
+        public ProductController(IRepository<Product> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var products = _repository.GetAll();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var product = _repository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            _repository.Add(product);
+            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return NoContent();
+        }
+    }
+}
