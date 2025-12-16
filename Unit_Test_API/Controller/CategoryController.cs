3c31efa4dcf16e6a1b70b6b1f68e87da11d16305
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-16 23:44:25 +0300

    feat(controller) :  created

---
+++ Unit_Test_API/Controller/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unit_Test_API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    // C# 12 Primary Constructor: Injects the repository directly into the class scope
+    public class CategoryController(IRepository<Category> repository) : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Category>> GetCategories()
+            => Ok(repository.GetAll());
+
+        [HttpGet("{id}")]
+        public ActionResult<Category> GetCategory(int id)
+            => repository.GetById(id) switch
+            {
+                null => NotFound(),
+                var category => Ok(category)
+            };
+
+        [HttpPost]
+        public ActionResult<Category> Create(Category category)
+        {
+            repository.Add(category);
+            return CreatedAtAction(nameof(GetCategory), new { id = category.Id }, category);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            repository.Delete(id);
+            return NoContent();
+        }
+    }
+}
